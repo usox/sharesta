e259826c8e7c0641ad826662531a8abe5eb670d9
Daniel Jakob <wolverine@usox.org>
2018-05-20 18:37:25 +0200

    Experimental: Replace mockery by hackmock dev-master
    Resolves #5

--- tests/RouterTest.hh
+++ tests/RouterTest.hh
@@ -3,219 +3,184 @@ namespace Usox\Sharesta;
 
 use function Facebook\FBExpect\expect;
 use HH\Lib\Str;
+use Usox\HackMock\MockBuilder;
 
 class RouterTest extends \PHPUnit_Framework_TestCase {
-
-	private ?\JsonSerializable $result;
-
-	private ?RequestInterface $request;
-
-	private ?ApiFactoryInterface $api_factory;
-
-	private ?ResponseInterface $response;
-
-	private ?Router $router;
 
 	private string $base_path = 'some-nice-path';
 
 	private string $default_result = 'roedlbroem';
 
-	public function setUp(): void {
-		$this->result = $this->createMock(\JsonSerializable::class);
-		// UNSAFE
-		$this->request = \Mockery::mock(RequestInterface::class);
-		$this->api_factory = \Mockery::mock(ApiFactoryInterface::class);
-		$this->response = \Mockery::mock(ResponseInterface::class);
-
-		$this->router = new Router(
-			$this->api_factory,
-			$this->request
-		);
-	}
-
 	public function testRouterMatchesRequestCorrectly(): void {
-		$this->router?->register('users/:id/:field', $this->getCallable());
+		$result = $this->getHackMockBuilder()->prototype(\JsonSerializable::class);
+		$api_factory_builder = $this->getHackMockBuilder()->prototype(ApiFactoryInterface::class);
+		$response_builder = $this->getHackMockBuilder()->prototype(ResponseInterface::class);
+		$request_builder = $this->getHackMockBuilder()->prototype(RequestInterface::class);
 
-		$this->createRequest('users/12/name', 'GET');
+		$response_builder
+			->expects('send')
+			->times(1);
 
-		// UNSAFE
-		$this->request
-			->shouldReceive('setRouteParameters')
-			->with(\Mockery::on(function ($value) {
-				return $value->toArray() == ['id' => 12, 'field' => 'name'];
-			}))
-			->once();
+		$result->expects('jsonSerialize')
+			->times(1)
+			->andReturn($this->default_result);
 
-		$this->result
-			?->expects(static::once())
-			?->method('jsonSerialize')
-			?->willReturn($this->default_result);
-
-		$this->api_factory
-			->shouldReceive('createResponse')
+		$api_factory_builder
+			->expects('createResponse')
 			->with(Router::HTTP_OK, Str\format('"%s"', $this->default_result))
-			->once()
-			->andReturn($this->response);
+			->times(1)
+			->andReturn($response_builder->build());
 
-		$this->response
-			->shouldReceive('send')
-			->once();
+		$request_builder
+			->expects('setRouteParameters')
+			->with(dict['id' => 12, 'field' => 'name'])
+			->times(1);
+		$request_builder
+			->expects('getRoute')
+			->times(1)
+			->andReturn('users/12/name');
+		$request_builder
+			->expects('getHttpMethod')
+			->times(1)
+			->andReturn('GET');
 
-		expect(
-			$this->router?->route($this->base_path)
-		)
-		->toBeNull();
+		$router = new Router(
+			$api_factory_builder->build(),
+			$request_builder->build()
+		);
+
+		$router->register('users/:id/:field', $result->build());
+
+		$router->route($this->base_path);
 	}
 
 	public function testRouterCatchesDefaultExceptionCorrectly(): void {
-		// UNSAFE
-		$this->request
-			->shouldReceive('getRoute')
-			->once()
+		$api_factory_builder = $this->getHackMockBuilder()->prototype(ApiFactoryInterface::class);
+		$response_builder = $this->getHackMockBuilder()->prototype(ResponseInterface::class);
+		$request_builder = $this->getHackMockBuilder()->prototype(RequestInterface::class);
+
+		$response_builder
+			->expects('send')
+			->times(1);
+
+		$request_builder
+			->expects('getRoute')
+			->times(1)
 			->andThrow(new \Exception());
 
-		$this->api_factory
-			->shouldReceive('createResponse')
+		$api_factory_builder
+			->expects('createResponse')
 			->with(Router::HTTP_INTERNAL_SERVER_ERROR, 'Internal server error')
-			->once()
-			->andReturn($this->response);
+			->times(1)
+			->andReturn($response_builder->build());
 
-		$this->response
-			->shouldReceive('send')
-			->once();
-		
-		expect(
-			$this->router?->route($this->base_path)
-		)
-		->toBeNull();
+		$router = new Router(
+			$api_factory_builder->build(),
+			$request_builder->build()
+		);
+
+		$router->route($this->base_path);
 	}
 
 	public function testRouterFailsToMatchRequestCorrectly(): void {
-		$this->router?->register('foo/:id/:field', $this->getCallable());
+		$result_builder = $this->getHackMockBuilder()->prototype(\JsonSerializable::class);
+		$api_factory_builder = $this->getHackMockBuilder()->prototype(ApiFactoryInterface::class);
+		$response_builder = $this->getHackMockBuilder()->prototype(ResponseInterface::class);
+		$request_builder = $this->getHackMockBuilder()->prototype(RequestInterface::class);
 
-		$this->createRequest('/api/users/12/name', 'GET');
+		$request_builder
+			->expects('getRoute')
+			->times(1)
+			->andReturn('/api/users/12/name');
 
-		// UNSAFE
-		$this->api_factory
-			->shouldReceive('createResponse')
+		$request_builder
+			->expects('getHttpMethod')
+			->times(1)
+			->andReturn('GET');
+
+		$response_builder
+			->expects('send')
+			->times(1);
+
+		$api_factory_builder
+			->expects('createResponse')
 			->with(Router::HTTP_NOT_FOUND, 'The requested resource was not found')
-			->once()
-			->andReturn($this->response);
+			->times(1)
+			->andReturn($response_builder->build());
 
-		$this->response
-			->shouldReceive('send')
-			->once();
+		$router = new Router(
+			$api_factory_builder->build(),
+			$request_builder->build()
+		);
 
-		expect(
-			$this->router?->route($this->base_path)
-		)
-		->toBeNull();
+		$router->register('foo/:id/:field', $result_builder->build());
+
+		$router->route($this->base_path);
 	}
 
 	public function testRouterMatchesSpecificHTTPMethodCorrectly(): void {
-		$this->router?->register('gettest/:id/:field', $this->getCallable(), 'GET');
-		$this->router?->register('posttest/:id/:field', $this->getCallable(), 'POST');
-		$this->router?->register('puttest/:id/:field', $this->getCallable(), 'PUT');
-		$this->router?->register('deltest/:id/:field', $this->getCallable(), 'DELETE');
+		$result_builder = $this->getHackMockBuilder()->prototype(\JsonSerializable::class);
+		$api_factory_builder = $this->getHackMockBuilder()->prototype(ApiFactoryInterface::class);
+		$response_builder = $this->getHackMockBuilder()->prototype(ResponseInterface::class);
+		$request_builder = $this->getHackMockBuilder()->prototype(RequestInterface::class);
 
-		// UNSAFE
-		$this->request
-			->shouldReceive('setRouteParameters')
-			->with(\Mockery::on(function ($value) {
-				return $value->toArray() == ['id' => 12, 'field' => 'name'];
-			}))
+		$result = $this->getHackMockBuilder()->prototype(\JsonSerializable::class);
+		$result->expects('jsonSerialize')
+			->times(4)
+			->andReturn($this->default_result);
+
+		$result = $result->build();
+
+		$request_builder
+			->expects('setRouteParameters')
+			->with(dict['id' => 12, 'field' => 'name'])
 			->times(4);
 
-		$this->api_factory
-			->shouldReceive('createResponse')
+		$response_builder
+			->expects('send')
+			->times(4);
+
+		$api_factory_builder
+			->expects('createResponse')
 			->with(Router::HTTP_OK, Str\format('"%s"', $this->default_result))
 			->times(4)
-			->andReturn($this->response);
+			->andReturn($response_builder->build());
 
-		$this->response
-			->shouldReceive('send')
-			->times(4);
+		$test = function (
+			string $route,
+			string $route_base,
+			string $method
+			) use (
+				$request_builder,
+				$api_factory_builder,
+				$result
+			) {
+			$request_builder
+				->expects('getRoute')
+				->times(1)
+				->andReturn($route);
+			$request_builder
+				->expects('getHttpMethod')
+				->times(1)
+				->andReturn($method);
 
-		$this->result
-			?->expects(static::exactly(4))
-			?->method('jsonSerialize')
-			?->willReturn($this->default_result);
+			$router = new Router(
+				$api_factory_builder->build(),
+				$request_builder->build()
+			);
 
-		$this->createRequest('gettest/12/name', 'GET');
+			$router->register($route_base.'/:id/:field', $result, $method);
 
-		expect($this->router?->route($this->base_path))->toBeNull();
-
-		$this->createRequest('posttest/12/name', 'POST');
-
-		expect($this->router?->route($this->base_path))->toBeNull();
-
-		$this->createRequest('puttest/12/name', 'PUT');
-
-		expect($this->router?->route($this->base_path))->toBeNull();
-
-		$this->createRequest('deltest/12/name', 'DELETE');
-
-		expect($this->router?->route($this->base_path))->toBeNull();
-	}
-
-	public function testRouterMatchesSpecificHTTPMethodCorrectlyUsingShortcuts(): void {
-		$this->router?->get('gettest/:id/:field', $this->getCallable());
-		$this->router?->post('posttest/:id/:field', $this->getCallable());
-		$this->router?->put('puttest/:id/:field', $this->getCallable());
-		$this->router?->delete('deltest/:id/:field', $this->getCallable());
-
-		// UNSAFE
-		$this->request
-			->shouldReceive('setRouteParameters')
-			->with(\Mockery::on(function ($value) {
-				return $value->toArray() == ['id' => 12, 'field' => 'name'];
-			}))
-			->times(4);
-
-		$this->api_factory
-			->shouldReceive('createResponse')
-			->with(Router::HTTP_OK, Str\format('"%s"', $this->default_result))
-			->times(4)
-			->andReturn($this->response);
-
-		$this->response
-			->shouldReceive('send')
-			->times(4);
-
-		$this->result
-			?->expects(static::exactly(4))
-			?->method('jsonSerialize')
-			?->willReturn($this->default_result);
-
-		$this->createRequest('gettest/12/name', 'GET');
-
-		expect($this->router?->route($this->base_path))->toBeNull();
-
-		$this->createRequest('posttest/12/name', 'POST');
-
-		expect($this->router?->route($this->base_path))->toBeNull();
-
-		$this->createRequest('puttest/12/name', 'PUT');
-
-		expect($this->router?->route($this->base_path))->toBeNull();
-
-		$this->createRequest('deltest/12/name', 'DELETE');
-
-		expect($this->router?->route($this->base_path))->toBeNull();
-	}
-
-	private function createRequest(string $route, string $http_method): void {
-		// UNSAFE
-		$this->request->shouldReceive('getRoute')->once()->andReturn($route);
-		$this->request->shouldReceive('getHttpMethod')->once()->andReturn($http_method);
-	}
-
-	private function getCallable(): (function(RequestInterface): \JsonSerializable) {
-		return function(RequestInterface $request): \JsonSerializable {
-			if ($this->result !== null) {
-				return $this->result;
-			}
-			throw new \Exception();
+			$router->route($this->base_path);
 		};
+
+		$test('gettest/12/name', 'gettest', 'GET');
+		$test('posttest/12/name', 'posttest', 'POST');
+		$test('puttest/12/name', 'puttest', 'PUT');
+		$test('deltest/12/name', 'deltest', 'DELETE');
+	}
+
+	public function getHackMockBuilder(): MockBuilder {
+		return new MockBuilder();
 	}
 }
